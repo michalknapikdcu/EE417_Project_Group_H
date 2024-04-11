5414b10d7cadd840b1c6ca2604538f786ebb8707
Louis FEVRE <louis.fevre2@mail.dcu.ie>
2024-04-11 12:32:44 +0100

    even more dynamic sensors reading

--- application/sensor-app/src/main/resources/static/js/fetchDataPrev.js
+++ application/sensor-app/src/main/resources/static/js/fetchDataPrev.js
@@ -14,60 +14,29 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
-/**
- * Initializes the widgets on the page.
- */
-function initWidget() {
-  document.getElementById('noise-widget').innerHTML = '<h2>Noise</h2>';
-  document.getElementById('temperature-widget').innerHTML = '<h2>Temperature</h2>';
-  document.getElementById('parking-widget').innerHTML = '<h2>Parking</h2>';
-}
+
 
 /**
- * Updates the last updated time for each widget.
+ * Fetches the sensor data from the server.
+ * @param {Object} data - The sensor data.
+ * @param {string} data.location - The location of the sensor.
+ * @param {number} data.lastReading - The last reading of the sensor.
+ * @param {string} data.unit - The unit of the sensor.
  */
-function endWidget() {
-  document.getElementById('noise-widget').innerHTML += '<p>Last Updated: <span id="noise-widget-update">' + new Date().toLocaleTimeString() + '</span></p>';
-  document.getElementById('temperature-widget').innerHTML += '<p>Last Updated: <span id="temperature-widget-update">' + new Date().toLocaleTimeString() + '</span></p>';
-  document.getElementById('parking-widget').innerHTML += '<p>Last Updated: <span id="parking-widget-update">' + new Date().toLocaleTimeString() + '</span></p>';
+function fetchData(data) {
+  if (!document.getElementById(data.name + '-widget')) {
+    var container = document.getElementsByClassName('container')[0];
+    var widget = document.createElement('div');
+    widget.id = data.name + '-widget';
+    widget.className = 'widget';
+    container.appendChild(widget);
+    widget.innerHTML = '<h2>' + data.name.charAt(0).toUpperCase() + data.name.slice(1) + '</h2>';
+    widget.innerHTML += '<p>Last Updated: <span id="noise-widget-update">' + new Date().toLocaleTimeString() + '</span></p>';
+  }
+  var widget = document.getElementById(data.name + '-widget');
+  widget.innerHTML += '<p>' + data.location + ' | available: <span id="' + data.location + '-' + data.name + '">' + data.lastReading + '</span> ' + data.unit + '</p>';
 }
 
-/**
- * Fetches noise data and updates the noise widget.
- * @param {Object} data - The noise data to be displayed.
- * @param {string} data.location - The location of the noise reading.
- * @param {number} data.lastReading - The last recorded noise reading.
- * @param {string} data.unit - The unit of noise measurement.
- */
-function fetchNoise(data) {
-  var widget = document.getElementById('noise-widget');
-  widget.innerHTML += '<p>' + data.location + ' noise level: <span id="' + data.location + '-noise">' + data.lastReading + '</span> ' + data.unit + '</p>';
-}
-
-/**
- * Fetches temperature data and updates the temperature widget.
- * @param {Object} data - The temperature data to be displayed.
- * @param {string} data.location - The location of the temperature reading.
- * @param {number} data.lastReading - The last recorded temperature reading.
- * @param {string} data.unit - The unit of temperature measurement.
- */
-function fetchTemperature(data) {
-  var widget = document.getElementById('temperature-widget');
-  widget.innerHTML = '<h2>Temperature</h2>';
-  widget.innerHTML += '<p>' + data.location + ' temperature: <span id="' + data.location + '-temperature">' + data.lastReading + '</span> ' + data.unit + '</p>';
-}
-
-/**
- * Fetches parking data and updates the parking widget.
- * @param {Object} data - The parking data to be displayed.
- * @param {string} data.location - The location of the parking reading.
- * @param {number} data.lastReading - The last recorded parking reading.
- */
-function fetchParking(data) {
-  var widget = document.getElementById('parking-widget');
-  widget.innerHTML = '<h2>Parking</h2>';
-  widget.innerHTML += '<p>' + data.location + ' | available: <span id="' + data.location + '-parking">' + data.lastReading + '</span></p>';
-}
 
 /**
  * Fetches all sensor data from the server.
@@ -76,21 +45,16 @@ function fetchParking(data) {
  */
 function fetchAllSensorData() {
   initWidget();
+  var container = document.getElementsByClassName('container')[0];
+  container.innerHTML = '';
   fetch('sensors')
     .then(response => response.json())
     .then(data => {
       data._embedded["sensorList"].forEach(sensor => {
-        if (sensor.name === 'noise') {
-          fetchNoise(sensor);
-        } else if (sensor.name === 'temperature') {
-          fetchTemperature(sensor);
-        } else if (sensor.name === 'parking') {
-          fetchParking(sensor);
-        }
+        fetchData(sensor);
       });
     })
     .finally(() => {
-      endWidget();
     })
     .catch(error => console.error('Error:', error));
 }
