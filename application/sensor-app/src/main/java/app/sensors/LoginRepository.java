af812ca38654c17426aa9f11093bcb7407306f88
siddharthbhat <65408005+siddharthbhat@users.noreply.github.com>
2024-04-13 19:59:05 +0100

    Update LoginRepository.java

--- application/sensor-app/src/main/java/app/sensors/LoginRepository.java
+++ application/sensor-app/src/main/java/app/sensors/LoginRepository.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface LoginRepository extends JpaRepository<Login, Long> {
     Login findByUsername(String username);
-    List<Login> findByRole(String access_level); // Method to find users by role
+    List<Login> findByRole(String role); // Method to find users by access level
 }
